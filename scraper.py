3c7ba4e78a0ad586cc0ac16d7afc737664e29b6e
Conner Stoebe <cbstoebe@gmail.com>
2024-02-09 17:01:28 -0800

    Update scraper.py
    COmpleted extract_next_links

--- scraper.py
+++ scraper.py
@@ -6,17 +6,24 @@ def scraper(url, resp):
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
+    # Check if the response is OK
+    if resp.status != 200:
+        return []
+
+    # Parse the content using BeautifulSoup
+    soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+
+    # Extract all the <a> tags
     links = []
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again
-    #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    for a_tag in soup.find_all('a', href=True):
+        href = a_tag['href']
+        # Convert relative URLs to absolute URLs
+        absolute_url = urljoin(resp.url, href)
+        # Append the absolute URL to the list
+        links.append(absolute_url)
+
+    return links
+
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
