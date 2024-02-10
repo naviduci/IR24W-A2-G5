280186fe445e9c16867d5c0d96722b6beffbeed5
luongp134 <80845739+luongp134@users.noreply.github.com>
2024-02-10 00:34:36 -0800

    some reordering

--- scraper.py
+++ scraper.py
@@ -4,17 +4,11 @@ from bs4 import BeautifulSoup
 
 
 # global variables
-# list containing every token found on every pages tokenized
-TokenList = []
-# Maxtokens is the highest number of tokens found on a url, and MaxURL is the
-# url which has the highest number of tokens
-MaxTokens = 0
-MaxURL = ""
-# this is a set containing every URL checked, not including fragments
-UniqueUrl = set()
-# a dictionary containing the subdomain as a key and the amount of pages in that
-# subdomain as the value
-Subdomains = dict()
+TokenList = [] # contain all token found on every tokenize pages
+MaxTokens = 0 # the highest number of tokens found on a url
+MaxURL = "" # the url which has the highest number of tokens
+UniqueUrl = set() # contain every URL checked excluding fragments
+Subdomains = dict() # contain the subdomain as a key and the amount of pages in that subdomain as the value
 # a counter that increments down to 1 before being reset, used in updating the
 # output file containing our report.
 updateOutput = 1500
@@ -43,55 +37,6 @@ def scraper(url, resp):
     else:
         pageCount -= 1
     return [link for link in links if is_valid(link)]
-
-def extract_next_links(url, resp):
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again
-    #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    if resp.status != 200:
-        return False
-    
-    webResponse = BeautifulSoup(resp.raw_response.content, 'html.parser')
-    # tokenizes the web contents and checks if the page has more tokens then
-    # the max. If it is greater the max number
-    # is updated and the MaxURL is changed to the new url
-    global MaxTokens
-    global MaxURL
-    urlTokens = tokenize(webResponse.getText())
-    
-    # compares current webpage's token count to the highest count to see if we
-    # have a new highest num of tokens
-    if len(urlTokens) > MaxTokens:
-        MaxTokens = len(urlTokens)
-        MaxURL = url
-
-    # takes the list of tokens created and adds them to the DBDictionary
-
-    #updateDBD(urlTokens)
-    tags = webResponse.find_all('a')
-    urlList = []
-    for tag in tags:
-        tempURL = tag.get('href')
-        # if the url is emtpy don't check it
-        if tempURL is None:
-            continue
-        # if it finds the fragment tag in url it takes it off, the it appends
-        # the url to our list of urls to search
-        possibleInd = tempURL.find('#')
-        if possibleInd != -1:
-            tempURL = tempURL[:possibleInd]
-            urlList.append(tempURL)
-        else:
-            urlList.append(tempURL)
-
-    return urlList
-
 
 def tokenize(resp):
     # Tokenizes a text file looking for an sequence of 2+ alphanumerics while
@@ -128,63 +73,6 @@ def tokenize(resp):
     updateDBD(urlTokens)
     return urlTokens
 
-
-def updateDBD(Tokens):
-    # Take list of tokens updates the DBDictionary to include these tokens
-    global TokenList
-    TokenList.extend(Tokens)
-
-
-def print50(wordList):
-    #prints the frequencies of the list of words that it is passed
-    freqList=Counter(wordList)
-    finalList = []
-
-    for word in freqList.most_common(50):
-        finalList.append(word[0])
-    return finalList
-
-
-
-def updateSubdomains(UniqueUrl):
-    #takes the set containing all unique urls and builds a dictionary
-    # and number of page as value
-    global Subdomains
-    Subdomains.clear()
-    for url in UniqueUrl:
-        parsed = urlparse(url)
-        if 'ics.uci.edu' in parsed.netloc.lower():
-            Subdomains[parsed.hostname] = Subdomains.get(parsed.hostname, 0) + 1
-
-
-def getOutput():
-    # returns a string with the answer to all four problems TO DO make it
-    # output to a textfile
-    global UniqueUrl, MaxURL, MaxTokens
-    output = "1. Number of unique pages found: " + str(len(UniqueUrl)) + "\n\n"
-    output += "2. Longest page in terms of number of words is " + str(MaxURL) \
-              + " with " + str(
-        MaxTokens) + " words total\n\n"
-    output += "3. 50 most common words in order of most frequent to least " \
-              "frequent are \n   "
-    commonWords = print50(TokenList)
-    for word in commonWords:
-        output += word + "\n  "
-    output += "\n4. Subdomains found: \n"
-    updateSubdomains(UniqueUrl)
-    for key, value in Subdomains.items():
-        output += "   subdomain name: " + str(key) + ", pages found: " \
-                  + str(value) + "\n"
-    try:
-        f = open("output.txt", "x")
-    except:
-        f = open("output.txt", "w")
-    finally:
-        f.write(output)
-        f.close()
-
-
-
 def is_valid(url):
     global UniqueUrl
 
@@ -194,8 +82,7 @@ def is_valid(url):
         if parsed.hostname==None or parsed.netloc==None:
             return False
         validDomains=[".ics.uci.edu","cs.uci.edu",".informatics.uci.edu",
-                      ".stat.uci.edu",\
-               "today.uci.edu/department/information_computer_sciences"]
+                      ".stat.uci.edu"]
         if parsed.scheme not in set(["http", "https"]) or (url.find("?") != -1)\
                 or (url.find("&") != -1):
             return False
@@ -229,3 +116,100 @@ def is_valid(url):
     except TypeError:
         print("TypeError for ", parsed)
         raise
+
+# helper methods
+
+def update_max_tokens(tokens, url):
+    # compares current webpage's token count to the highest count to see if we
+    # have a new highest num of tokens
+    global MaxTokens
+    global MaxURL
+    if len(tokens) > MaxTokens:
+        MaxTokens = len(tokens)
+        MaxURL = url
+
+def extract_links_from_tags(tags):
+    # takes the list of tokens created and adds them to the DBDictionary
+    #updateDBD(urlTokens)
+    url_list = []
+    for tag in tags:
+        temp_url = tag.get('href')
+        # if it finds the fragment tag in url it takes it off, the it appends
+        # the url to our list of urls to search
+        if temp_url is not None:
+            possible_ind = temp_url.find('#')
+            if possible_ind != -1:
+                temp_url = temp_url[:possible_ind]
+            url_list.append(temp_url)
+    return url_list
+
+def updateDBD(Tokens):
+    # Take list of tokens updates the DBDictionary to include these tokens
+    global TokenList
+    TokenList.extend(Tokens)
+
+
+def print50(wordList):
+    #prints the frequencies of the list of words that it is passed
+    freqList=Counter(wordList)
+    finalList = []
+
+    for word in freqList.most_common(50):
+        finalList.append(word[0])
+    return finalList
+
+def updateSubdomains(UniqueUrl):
+    #takes the set containing all unique urls and builds a dictionary
+    # and number of page as value
+    global Subdomains
+    Subdomains.clear()
+    for url in UniqueUrl:
+        parsed = urlparse(url)
+        if 'ics.uci.edu' in parsed.netloc.lower():
+            Subdomains[parsed.hostname] = Subdomains.get(parsed.hostname, 0) + 1
+           
+def getOutput():
+    # returns a string with the answer to all four problems 
+    # TODO: make it output to a textfile
+    global UniqueUrl
+    global MaxURL
+    global MaxTokens
+    output = "1. Number of unique pages found: " + str(len(UniqueUrl)) + "\n\n"
+    output += "2. Longest page in terms of number of words is " + str(MaxURL) \
+              + " with " + str(MaxTokens) + " words total\n\n"
+    output += "3. 50 most common words in order of most frequent to least " \
+              "frequent are \n   "
+    commonWords = print50(TokenList)
+    for word in commonWords:
+        output += word + "\n  "
+    output += "\n4. Subdomains found: \n"
+    updateSubdomains(UniqueUrl)
+    for key, value in Subdomains.items():
+        output += "   subdomain name: " + str(key) + ", pages found: " \
+                  + str(value) + "\n"
+    try:
+        f = open("output.txt", "x")
+    except:
+        f = open("output.txt", "w")
+    finally:
+        f.write(output)
+        f.close()
+ 
+def extract_next_links(url, resp):
+    # Implementation required.
+    # url: the URL that was used to get the page
+    # resp.url: the actual url of the page
+    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+    # resp.error: when status is not 200, you can check the error here, if needed.
+    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
+    #         resp.raw_response.url: the url, again
+    #         resp.raw_response.content: the content of the page!
+    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+    if resp.status != 200:
+        return False
+    
+    webResponse = BeautifulSoup(resp.raw_response.content, 'html.parser')
+    urlTokens = tokenize(webResponse.getText())
+    update_max_tokens(urlTokens, url)
+
+    return extract_links_from_tags(webResponse.find_all('a'))
