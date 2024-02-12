42b8e22f1ec0b34864cc303f9ebcaf195b1115df
luongp134 <80845739+luongp134@users.noreply.github.com>
2024-02-11 17:45:31 -0800

    update master for efficiency, also added some comments

--- scraper.py
+++ scraper.py
@@ -1,21 +1,21 @@
 import re
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
 import nltk
-import requests
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from collections import Counter
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import requests
 nltk.download('stopwords')
 
 # global variables
-TokenList = [] # contain all token found on every tokenize pages
-MaxTokens = 0 # the highest number of tokens found on a url
-MaxURL = "" # the url which has the highest number of tokens
-UniqueUrl = set() # contain every URL checked excluding fragments
-Subdomains = dict() # contain the subdomain as a key and the amount of pages in that subdomain as the value
+tokenize_list = [] # contain all token found on every tokenize pages
+max_url_token = 0 # the highest number of tokens found on a url
+max_url = "" # the url which has the highest number of tokens
+unique_set = set() # contain every URL checked excluding fragments
+subdomains = dict() # contain the subdomain as a key and the amount of pages in that subdomain as the value
 data_processed = 0 # the currect data processed
-data_threshold = 5000000  # Set the data threshold (e.g., 5 MB)
+data_threshold = 2000000  # Set the data threshold (e.g., 2 MB)
 frontier_empty = False # output file Output.txt containing our report.
 visited_domains_robots = {}
 
@@ -73,30 +73,25 @@ def get_fingerprint(content, n=3, functions=hash):
 #extra credit 2 functions (end)
 
 def scraper(url, resp):
-    # global respStats
-    if resp.status != 200 or not resp.raw_response.content:
-        return []
-    
-    # check content within a page of code 200
-
-    links = extract_next_links(url, resp)
-
-    # TODO: check if this works as intended
-    # global updateOutput
-    
     global data_processed
     global data_threshold
     global frontier_empty
+    # check content within a page of code 200
+    if resp.status != 200 or not resp.raw_response.content:
+        return []
+
+    links = extract_next_links(url, resp)
+    
     # keep track of the data being processed, update at data processed threshold
     data_processed = len(get_content_from_response(resp)) if get_content_from_response(resp) else 0
     if data_processed >= data_threshold:
         getOutput()
         data_processed = 0
-    
+
     if not links:
         frontier_empty = True
 
-    # Call getOutput() only if frontier is empty
+    # Update output.txt through getOutput() if frontier is empty
     if frontier_empty:
         getOutput()
         frontier_empty = False
@@ -136,15 +131,14 @@ def is_valid(url):
     
     try:
         parsed = urlparse(url)
+        validDomains=[".ics.uci.edu","cs.uci.edu",".informatics.uci.edu",
+                      ".stat.uci.edu"]
+        if parsed.hostname == None or parsed.netloc == None:
+            return False
         if parsed.scheme not in set(["http", "https"]) or (url.find("?") != -1)\
                 or (url.find("&") != -1):
             return False
-        if parsed.hostname == None or parsed.netloc == None:
-            # print("Hostname or Netloc is None")
-            return False
-        validDomains = [".ics.uci.edu","cs.uci.edu",".informatics.uci.edu",".stat.uci.edu"]
-
-        # exclude list of file-extension urls along with any URLs that contain numeric date-like pattern
+        
         if any(dom in parsed.hostname for dom in validDomains) \
             and not re.search(r"(css|js|bmp|gif|jpe?g|ico"
                               + r"|png|tiff?|mid|mp2|mp3|mp4"
@@ -163,13 +157,10 @@ def is_valid(url):
                              r'[0-9]{2}-[0-9]{1,2}|\/[0-9]{1,2}-(19|20)[0-9]{2}|'
                              r'[0-9]{1,2}-[0-9]{1,2}-(19|20)[0-9]{2}',
                              parsed.path.lower()):
-            
-            if url in UniqueUrl:
-                 # print("URL already in UniqueUrl set")
+            if url in unique_set:
                 return False
             else:
-                UniqueUrl.add(url)
-                # print("URL added to UniqueUrl set")
+                unique_set.add(url)
                 return True
         else:
             return False
@@ -185,88 +176,78 @@ def get_content_from_response(resp):
 def update_max_tokens(tokens, url):
     # compares current webpage's token count to the highest count to see if we
     # have a new highest num of tokens
-    global MaxTokens
-    global MaxURL
-    if len(tokens) > MaxTokens:
-        MaxTokens = len(tokens)
-        MaxURL = url
+    global max_url_token
+    global max_url
+    if len(tokens) > max_url_token:
+        max_url_token = len(tokens)
+        max_url = url
 
+# updated for efficiency
 def extract_links_from_tags(tags):
     # takes the list of tokens created and adds them to the DBDictionary
     #updateDBD(urlTokens)
-    url_list = []
-    for tag in tags:
-        temp_url = tag.get('href')
-        # if it finds the fragment tag in url it takes it off, the it appends
-        # the url to our list of urls to search
-        if temp_url is not None:
-            possible_ind = temp_url.find('#')
-            if possible_ind != -1:
-                temp_url = temp_url[:possible_ind]
-            url_list.append(temp_url)
+    url_list = [tag.get('href').split('#')[0] for tag in tags if tag.get('href')]
     return url_list
 
 def updateDBD(Tokens):
     # Take list of tokens updates the DBDictionary to include these tokens
-    global TokenList
-    TokenList.extend(Tokens)
+    global tokenize_list
+    tokenize_list.extend(Tokens)
 
-
-def print50(wordList):
+# update for efficiency
+def print_top_50(wordList):
     #prints the frequencies of the list of words that it is passed
-    freqList=Counter(wordList)
-    finalList = []
-
-    for word in freqList.most_common(50):
-        finalList.append(word[0])
+    freqList = Counter(wordList)
+    finalList = [word for word, _ in freqList.most_common(50)]
     return finalList
 
-def updateSubdomains(UniqueUrl):
+def update_subdomains(unique_set):
     #takes the set containing all unique urls and builds a dictionary
     # and number of page as value
-    global Subdomains
-    Subdomains.clear()
-    for url in UniqueUrl:
+    global subdomains
+    subdomains.clear()
+    for url in unique_set:
         parsed = urlparse(url)
         if 'ics.uci.edu' in parsed.netloc.lower():
-            Subdomains[parsed.hostname] = Subdomains.get(parsed.hostname, 0) + 1
+            subdomains[parsed.hostname] = subdomains.get(parsed.hostname, 0) + 1
            
 def getOutput():
     # returns a string with the answer to all four problems 
     # TODO: make it output to a textfile
-    global UniqueUrl
-    global MaxURL
-    global MaxTokens
-    output = "1. Number of unique pages found: " + str(len(UniqueUrl)) + "\n\n"
-    output += "2. Longest page in terms of number of words is " + str(MaxURL) \
-              + " with " + str(MaxTokens) + " words total\n\n"
-    output += "3. 50 most common words in order of most frequent to least " \
-              "frequent are \n   "
-    commonWords = print50(TokenList)
-    for word in commonWords:
-        output += word + "\n  "
-    output += "\n4. Subdomains found: \n"
-    # Sort subdomains by name
-    updateSubdomains(UniqueUrl)
-    sorted_subdomains = sorted(Subdomains.items(), key=lambda x: x[0])
+    global unique_set
+    global max_url
+    global max_url_token
+    output = ""
+    
+    # Problem 1: Number of unique pages found
+    output += f"1. Number of unique pages found: {len(unique_set)}\n\n"
+
+    # Problem 2: Longest page in terms of number of words
+    output += f"2. Longest page: {max_url} with {max_url_token} words total\n\n"
+
+    # Problem 3: 50 most common words
+    output += "3. 50 most common words in order of most frequent to least frequent are:\n   "
+    commonWords = print_top_50(tokenize_list)
+    output += "\n  ".join(commonWords) + "\n"
+
+    # Problem 4: Subdomains found
+    update_subdomains(unique_set)
+    sorted_subdomains = sorted(subdomains.items(), key=lambda x: x[0])
+    output += "4. Subdomains found:\n"
     for key, value in sorted_subdomains:
-        output += f"   subdomain name: {key}, pages found: {value}\n"
-    try:
-        f = open("output.txt", "x")
-    except Exception as e:
-        print(f'output.txt exist, updating file...')
-        f = open("output.txt", "w")
-    finally:
-        f.write(output)
-        f.close()
+        output += f"   Subdomain name: {key}, Pages found: {value}\n"
+
+    # Write output to a text file
+    with open("output.txt", "w") as file:
+        file.write(output)
+
+    print("Output has been written to output.txt file.")
  
 def tokenize(resp):
-    # Tokenizes a text file looking for an sequence of 2+ alphanumerics while
+    # Tokenizes a text file looking for an sequence of alphanumerics while
     # ignoring stop words
+    # exclusionWords exclude from the list of tokens. These include months and days
     urlTokens = []
-    # exclusionWords is a list of words that we dont want to include in out
-    # list of tokens. These include months and
-    # days, which appear in disproportionatly high numbers
     exclusionWords = {'january', 'jan', 'feb', 'february', 'march', 'mar',
                       'april', 'apr', 'may', 'june', 'jun', 'jul', 'july', 'aug'
                       'august', 'september', 'sept', 'aug', 'august', 'october',
@@ -275,10 +256,9 @@ def tokenize(resp):
                       'thurs', 'friday', 'fri', 'sat', 'saturday', 'sun',
                       'sunday'}
 
-    # tokenizes with the pattern '[a-z]{2,}' which finds two letters or more.
-    # We excluded numbers because there were
-    # no instances where we found numbers to have important meaning
-    myTokenizer = RegexpTokenizer('[a-z]{2,}')
+    # tokenizes with the pattern 'r'\w+'' which finds complete word.
+    # We excluded numbers to reduce extracting low value information
+    myTokenizer = RegexpTokenizer(r'\w+') # instead of '[a-z]{2,}'
     tempTokens = myTokenizer.tokenize(resp)
     sw = stopwords.words('english')
 
@@ -288,8 +268,7 @@ def tokenize(resp):
         checkToken = tokens.lower()
         if checkToken not in sw and checkToken not in exclusionWords:
             urlTokens.append(checkToken)
-        else:
-            continue
+
     # updataDBD adds all tokens found on this page to our master list
     # containing all tokens found on all pages
     updateDBD(urlTokens)
