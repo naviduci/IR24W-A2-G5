d209347405b54c059db923567c4adecf8dbb122d
luongp134 <80845739+luongp134@users.noreply.github.com>
2024-02-13 13:46:41 -0800

    small update

--- scraper.py
+++ scraper.py
@@ -88,13 +88,13 @@ def scraper(url, resp):
         getOutput()
         data_processed = 0
 
-    if not links:
-        frontier_empty = True
-
-    # Update output.txt through getOutput() if frontier is empty
-    if frontier_empty:
-        getOutput()
-        frontier_empty = False
+    # this is added into the worker.py
+    # if not links:
+        # frontier_empty = True
+    # Update holder.txt through getOutput() if frontier is empty
+    # if frontier_empty:
+        # getOutput()
+        # frontier_empty = False
     
     # checks the list of urls found on a page using the is_valid function to
     # decide whether or not to return each url
@@ -118,7 +118,11 @@ def extract_next_links(url, resp):
         return []
     
     soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
-    urlTokens = tokenize(soup.getText())
+    text_content = soup.get_text()
+    urlTokens = tokenize(text_content)
+    
+    if (urlTokens == []):
+        return []
     update_max_tokens(urlTokens, url)
     
     return extract_links_from_tags(soup.find_all('a'))
@@ -127,10 +131,11 @@ def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
-    global UniqueUrl
+    global visit_count
     
     try:
-        parsed = urlparse(url)
+        url_without_fragment = urldefrag(url).url
+        parsed = urlparse(url_without_fragment)
         validDomains=[".ics.uci.edu","cs.uci.edu",".informatics.uci.edu",
                       ".stat.uci.edu"]
         if parsed.hostname == None or parsed.netloc == None:
@@ -139,28 +144,40 @@ def is_valid(url):
                 or (url.find("&") != -1):
             return False
         
-        if any(dom in parsed.hostname for dom in validDomains) \
-            and not re.search(r"(css|js|bmp|gif|jpe?g|ico"
-                              + r"|png|tiff?|mid|mp2|mp3|mp4"
-                              + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-                              + r"|ps|eps|tex|txt|ppt|pptx|doc|docx|xls|xlsx|names"
-                              + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-                              + r"|epub|dll|cnf|tgz|sha1|php|z"
-                              + r"|thmx|mso|arff|rtf|jar|csv"
-                              + r"|rm|smil|wmv|swf|wma|zip|rar|gz|ppt|pptx|ppsx"
-                              + r"|january|february|march|april|may|june|july"
-                              + r"|august|september|october|november|december"
-                              + r"|jan|feb|mar|apr|jun|jul|aug|sep|oct|nov|dec"
-                              + r"|docs|docx|css|js|blog|page|calendar|archive|"
-                                r"events|event|date)", parsed.path.lower())\
-            and not re.match(r'\/(19|20)[0-9]{2}/|\/(19|20)[0-9]{2}$|\/(19|20)'
+        # Exclude URLs that match the following criteria:
+        # 1. Include a valid domain from the `validDomains` list.
+        # 2. Do not include certain file extensions or paths indicating non-content URLs.
+        # 3. Do not include months or mentioned of calendar in the URLs 
+        # 3. Do not match certain date or event-related patterns **REMOVED FOR TESTING**.
+        """and not re.match(r'\/(19|20)[0-9]{2}/|\/(19|20)[0-9]{2}$|\/(19|20)'
+                             r'[0-9]{2}-[0-9]{1,2}|\/[0-9]{1,2}-(19|20)[0-9]{2}|'
+                             r'[0-9]{1,2}-[0-9]{1,2}-(19|20)[0-9]{2}',
+                             parsed.path.lower())
+            
+                              """
+        if any(domain in parsed.hostname for domain in validDomains) \
+                and not re.search(r"(css|js|bmp|gif|jpe?g|ico"
+                                  r"|png|tiff?|mid|mp2|mp3|mp4"
+                                  r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+                                  r"|ps|eps|tex|txt|ppt|pptx|doc|docx|xls|xlsx|names"
+                                  r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+                                  r"|epub|dll|cnf|tgz|sha1|php|z|odc"
+                                  r"|thmx|mso|arff|rtf|jar|csv|docs|page"
+                                  r"|rm|smil|wmv|swf|wma|zip|rar|gz)", parsed.path.lower())\
+                and not re.match(r'\/(19|20)[0-9]{2}/|\/(19|20)[0-9]{2}$|\/(19|20)'
                              r'[0-9]{2}-[0-9]{1,2}|\/[0-9]{1,2}-(19|20)[0-9]{2}|'
                              r'[0-9]{1,2}-[0-9]{1,2}-(19|20)[0-9]{2}',
                              parsed.path.lower()):
-            if url in unique_set:
+
+            if url_without_fragment in visit_count and visit_count[url_without_fragment] >= 3:
+                return False  # Do not crawl if already visited more than 3 times
+
+            if url_without_fragment in unique_set:
+                visit_count[url_without_fragment] += 1
                 return False
             else:
-                unique_set.add(url)
+                unique_set.add(url_without_fragment)
+                visit_count[url_without_fragment] = 1
                 return True
         else:
             return False
@@ -182,7 +199,6 @@ def update_max_tokens(tokens, url):
         max_url_token = len(tokens)
         max_url = url
 
-# updated for efficiency
 def extract_links_from_tags(tags):
     # takes the list of tokens created and adds them to the DBDictionary
     #updateDBD(urlTokens)
@@ -194,7 +210,6 @@ def updateDBD(Tokens):
     global tokenize_list
     tokenize_list.extend(Tokens)
 
-# update for efficiency
 def print_top_50(wordList):
     #prints the frequencies of the list of words that it is passed
     freqList = Counter(wordList)
@@ -202,46 +217,46 @@ def print_top_50(wordList):
     return finalList
 
 def update_subdomains(unique_set):
-    #takes the set containing all unique urls and builds a dictionary
-    # and number of page as value
+    # Takes the set containing all unique urls and builds a dictionary
+    # with subdomains as keys and the number of occurrences as values
     global subdomains
     subdomains.clear()
     for url in unique_set:
         parsed = urlparse(url)
-        if 'ics.uci.edu' in parsed.netloc.lower():
+        if parsed.netloc.lower().endswith('.uci.edu'):
             subdomains[parsed.hostname] = subdomains.get(parsed.hostname, 0) + 1
            
 def getOutput():
     # returns a string with the answer to all four problems 
-    # TODO: make it output to a textfile
+    # TODO: create a textfile
     global unique_set
     global max_url
     global max_url_token
-    output = ""
+    holder = ""
     
     # Problem 1: Number of unique pages found
-    output += f"1. Number of unique pages found: {len(unique_set)}\n\n"
+    holder += f"1. Number of unique pages found: {len(unique_set)}\n\n"
 
     # Problem 2: Longest page in terms of number of words
-    output += f"2. Longest page: {max_url} with {max_url_token} words total\n\n"
+    holder += f"2. Longest page: {max_url} with {max_url_token} words total\n\n"
 
     # Problem 3: 50 most common words
-    output += "3. 50 most common words in order of most frequent to least frequent are:\n   "
+    holder += "3. 50 most common words in order of most frequent to least frequent are:\n   "
     commonWords = print_top_50(tokenize_list)
-    output += "\n  ".join(commonWords) + "\n"
+    holder += "\n  ".join(commonWords) + "\n"
 
     # Problem 4: Subdomains found
     update_subdomains(unique_set)
     sorted_subdomains = sorted(subdomains.items(), key=lambda x: x[0])
-    output += "4. Subdomains found:\n"
+    holder += "\n4. Subdomains found:\n"
     for key, value in sorted_subdomains:
-        output += f"   Subdomain name: {key}, Pages found: {value}\n"
+        holder += f"   Subdomain name: {key}, Pages found: {value}\n"
 
-    # Write output to a text file
-    with open("output.txt", "w") as file:
-        file.write(output)
+    # Write holder to a text file
+    with open("result.txt", "w") as file:
+        file.write(holder)
 
-    print("Output has been written to output.txt file.")
+    print("Result has been written to result.txt file.")
  
 def tokenize(resp):
     # Tokenizes a text file looking for an sequence of alphanumerics while
@@ -256,9 +271,9 @@ def tokenize(resp):
                       'thurs', 'friday', 'fri', 'sat', 'saturday', 'sun',
                       'sunday'}
 
-    # tokenizes with the pattern 'r'\w+'' which finds complete word.
+    # tokenizes with the pattern r'\b[a-zA-Z]{3,}\b' which finds tokens of 2 or more characters.
     # We excluded numbers to reduce extracting low value information
-    myTokenizer = RegexpTokenizer(r'\w+') # instead of '[a-z]{2,}'
+    myTokenizer = RegexpTokenizer(r'\b[a-zA-Z]{3,}\b') # instead of '[a-z]{2,}'
     tempTokens = myTokenizer.tokenize(resp)
     sw = stopwords.words('english')
 
@@ -272,4 +287,10 @@ def tokenize(resp):
     # updataDBD adds all tokens found on this page to our master list
     # containing all tokens found on all pages
     updateDBD(urlTokens)
+    
+    # Check if the number of tokens is less than the minimum required
+    min_tokens = 141
+    if len(resp) < min_tokens:
+        return []  # Return en empty list if the page doesn't have enough tokens
+
     return urlTokens
